c57c6f7999ee6558640f6bf186b47a6ab592127c
unionjackmasa <unionjackmasa@gmail.com>
2025-06-20 22:23:28 +0900

    [fix]スタート時のコード修正

--- Assets/C#/start.cs
+++ Assets/C#/start.cs
@@ -5,9 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class start : MonoBehaviour
 {
+    [SerializeField] GameObject _canvas;
     // Start is called before the first frame update
     public void StartBottan()
     {
         SceneManager.LoadScene("restrat");
+        GManager.instance.score = 0;
+        GManager.instance.mileagea = 0;
+        _canvas.SetActive(false);
     }
 }
